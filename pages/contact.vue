7f83833f05257181f911e8d4593ea01fad11b62b
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-02-19 11:53:46 +0100

    feat: add information to the contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,49 @@
 <template>
-  <p>Contact</p>
+  <div class="relative isolate px-6 py-14 pb-16 lg:px-8">
+    <div class="mx-auto max-w-2xl py-8 sm:py-12 lg:py-16">
+      <div>
+        <h1
+          class="text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-6xl"
+        >
+          Contact us
+        </h1>
+        <p class="mt-8 text-lg leading-8 text-gray-700">
+          Need to get in touch with us? Here's how:
+        </p>
+        <p class="mt-2 text-lg leading-8 text-gray-700">
+          <span class="font-bold">Classic Sportscar Centre</span>
+        </p>
+        <p class="text-lg leading-8 text-gray-700">Haarlem, The Netherlands</p>
+        <p class="mt-4 text-lg leading-8 text-gray-700">
+          <span class="font-semibold">Phone: </span
+          ><a class="underline hover:text-blue-800" href="tel:+31653728462"
+            >+31 6 53 72 84 62</a
+          >
+        </p>
+        <p class="text-lg leading-8 text-gray-700">
+          <span class="font-semibold">Email: </span
+          ><a
+            class="underline hover:text-blue-800"
+            href="mailto:info@classicsportscarcentre.com"
+            >info@classicsportscarcentre.com</a
+          >
+        </p>
+        <p class="text-lg leading-8 text-gray-700">
+          <span class="font-semibold">Facebook: </span
+          ><a
+            class="underline hover:text-blue-800"
+            rel="nofollow"
+            target="blank"
+            href="https://www.facebook.com/p/Classic-Sportscar-Centre-100045209156136/"
+            >facebook.com</a
+          >
+        </p>
+        <p class="mt-4 text-lg leading-8 text-gray-700">
+          For inquiries, appointments, or any other questions, feel free to
+          reach out to us using the contact information provided above. We look
+          forward to hearing from you!
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
